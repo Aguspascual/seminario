440cb0fe45ee3f37f136906a255f20de2aab2cd7
Agustin Pascual <aguspascual2001@gmail.com>
2026-02-06 16:21:09 -0300

    Resolve conflicts in AppRoutes.jsx

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,23 +1,6 @@
 import React, { Suspense, lazy } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-<<<<<<< HEAD
-import HomePage from '../pages/Home';
-import Login from '../pages/Login';
-import Usuarios from '../pages/Usuarios';
-import RecuperarPassword from '../pages/RecuperarPassword';
-import Proveedores from '../pages/Proveedores';
-import Maquinaria from '../pages/Maquinaria';
-import Capacitaciones from '../pages/Capacitaciones';
-import Auditorias from '../pages/Auditorias';
-import Areas from '../pages/Areas';
-import Reportes from '../pages/Reportes';
-import CambiarContraseña from '../pages/CambiarContraseña';
-import MiPerfil from '../pages/MiPerfil';
-import Mensajes from '../pages/Mensajes';
-
-=======
 import Loader from '../components/Loader';
->>>>>>> origin/master
 import PrivateRoute from '../components/PrivateRoute';
 
 // Lazy load pages
@@ -32,6 +15,7 @@ const Areas = lazy(() => import('../pages/Areas'));
 const Reportes = lazy(() => import('../pages/Reportes'));
 const CambiarContraseña = lazy(() => import('../pages/CambiarContraseña'));
 const MiPerfil = lazy(() => import('../pages/MiPerfil'));
+const Mensajes = lazy(() => import('../pages/Mensajes'));
 
 function AppRoutes() {
   return (
@@ -42,23 +26,6 @@ function AppRoutes() {
           <Route path="/login" element={<Login />} />
           <Route path="/recuperar" element={<RecuperarPassword />} />
 
-<<<<<<< HEAD
-        {/* Rutas Privadas */}
-        <Route element={<PrivateRoute />}>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/usuarios" element={<Usuarios />} />
-          <Route path="/proveedores" element={<Proveedores />} />
-          <Route path="/maquinaria" element={<Maquinaria />} />
-          <Route path="/capacitaciones" element={<Capacitaciones />} />
-          <Route path="/auditorias" element={<Auditorias />} />
-          <Route path="/areas" element={<Areas />} />
-          <Route path="/reportes" element={<Reportes />} />
-          <Route path="/mensajes" element={<Mensajes />} />
-          <Route path="/cambiarContraseña" element={<CambiarContraseña />} />
-          <Route path="/mi-perfil" element={<MiPerfil />} />
-        </Route>
-      </Routes>
-=======
           {/* Rutas Privadas */}
           <Route element={<PrivateRoute />}>
             <Route path="/" element={<HomePage />} />
@@ -68,12 +35,12 @@ function AppRoutes() {
             <Route path="/auditorias" element={<Auditorias />} />
             <Route path="/areas" element={<Areas />} />
             <Route path="/reportes" element={<Reportes />} />
+            <Route path="/mensajes" element={<Mensajes />} />
             <Route path="/cambiarContraseña" element={<CambiarContraseña />} />
             <Route path="/mi-perfil" element={<MiPerfil />} />
           </Route>
         </Routes>
       </Suspense>
->>>>>>> origin/master
     </BrowserRouter>
   );
 }
