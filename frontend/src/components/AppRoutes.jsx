e4cdce95a04e2d3187309da9445b42e79b303025
Agustin Pascual <aguspascual2001@gmail.com>
2026-02-06 17:26:09 -0300

    Update AppRoutes.jsx

---
+++ frontend/src/components/AppRoutes.jsx
@@ -0,0 +1,54 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ProtectedRoute from './ProtectedRoute'; //proteccion de rutas
+import HomePage from '../pages/Home';
+import Login from '../pages/Login';
+import Usuarios from '../pages/Usuarios';
+import RecuperarPassword from '../pages/RecuperarPassword';
+import Proveedores from '../pages/Proveedores';
+import Maquinaria from '../pages/Maquinaria';
+import Capacitaciones from '../pages/Capacitaciones';
+import Auditorias from '../pages/Auditorias';
+import Areas from '../pages/Areas';
+import Reportes from '../pages/Reportes';
+import CambiarContraseña from '../pages/CambiarContraseña';
+import MiPerfil from '../pages/MiPerfil';
+import Legal from '../pages/Legal';
+
+
+
+function AppRoutes({ user }) {
+    return (
+        <BrowserRouter>
+            <Routes>
+                {/* Rutas Públicas */}
+                <Route path="/" element={<Login />} />
+                <Route path="/recuperar" element={<RecuperarPassword />} />
+
+                {/* Rutas Privadas para los logueados*/}
+                <Route element={<ProtectedRoute user={user} />}>
+                    <Route path="/home" element={<HomePage user={user} />} />
+                    <Route path="/maquinaria" element={<Maquinaria />} />
+                    <Route path="/reportes" element={<Reportes />} />
+                    <Route path="/mi-perfil" element={<MiPerfil />} />
+                    <Route path="/cambiarContraseña" element={<CambiarContraseña />} />
+                </Route>
+
+                {/* --- 3. Rutas para Planta (Solo admin y Rol B) --- */}
+                <Route element={<ProtectedRoute user={user} allowedRoles={['admin', 'B']} />}>
+                    <Route path="/legal" element={<Legal />} />
+                    <Route path="/proveedores" element={<Proveedores />} />
+                    <Route path="/auditorias" element={<Auditorias />} />
+                    <Route path="/areas" element={<Areas />} />
+                    <Route path="/capacitaciones" element={<Capacitaciones />} />
+                </Route>
+
+                <Route element={<ProtectedRoute user={user} allowedRoles={['admin']} />}>
+                    <Route path="/usuarios" element={<Usuarios />} />
+                </Route>
+
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default AppRoutes;
